3ff30e66f0d50a692e5abb24d32fc0933541ec43
Rafael Facchin <rafaelbcfacchin@gmail.com>
2022-12-21 16:09:57 -0300

    Implementação da função de cálculo da estratégia de POV.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PercentageOfVolume
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Leitura da entrada:
+            Console.Write("Digite o total negociado: ");
+            int.TryParse(Console.ReadLine(), out int totalNegociado);
+            Console.WriteLine(totalNegociado);
+            Console.Write("Digite a porcentagem desejada, se for 10% digite 0,1: ");
+            decimal.TryParse(Console.ReadLine(), out decimal porcentagem);
+            Console.WriteLine(porcentagem);
+
+            //Cálculo da Percentage of Volume:
+            int pov = funcaoRetornaQuantidadePOV(porcentagem, totalNegociado);
+
+            //Exibição do resultado:
+            Console.WriteLine($"A estratégia deverá realizar {pov} execuções.");
+        }
+
+        private static int funcaoRetornaQuantidadePOV(decimal porcentagem, int totalNegociado)
+            => (int)Math.Floor((porcentagem * totalNegociado) / ((decimal)1.0 - porcentagem));
+    }
+}
